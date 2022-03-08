44f2cd36560243aaa6c8d810ed4ea0e64c2850f4
Francisco Ponce <francisco.ponce.dev@gmail.com>
2022-03-08 19:51:04 -0300

    Comments API Working. Not the ideal way, but it is ok.

--- test/counter.test.js
+++ test/counter.test.js
@@ -1,7 +1,7 @@
 import pokesCounter from './itemscountMock.js';
 
 describe('Testing items counter function', () => {
-  test('Count movies', () => {
+  test('Count Pokemons', () => {
     const items = ['pok1', 'pok2', 'pok3'];
     const itesmLength = items.length;
     const res = pokesCounter(items);
